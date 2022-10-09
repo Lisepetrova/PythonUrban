4a6b2d2516ebf0a1a517552efa5de60828e74d80
Lisepetrova <115191944+Lisepetrova@users.noreply.github.com>
2022-10-09 18:44:56 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -4,6 +4,6 @@ def get_mean_green_index(list_index) -> float:
     mean_index = 0
     mean_index = sum(list_index)/len(list_index)
     
-    return mean_index 
+    return round(mean_index,2)
 
 print(get_mean_green_index(list_index)) 
